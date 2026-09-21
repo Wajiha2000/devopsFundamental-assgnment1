802e5446a84505339d11e5f9d26bb581571ab3ac
wajiha <wajihaahmed1225@gmail.com>
2026-09-22 00:37:33 +0500

    Add basic Node.js application

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const server = http.createServer((req, res) => {
     if (req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("DevOps Demo Application is running!");
-    } else if (req.url === "/health") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ status: "healthy" }));
     } else {
         res.writeHead(404);
         res.end("Not Found");
