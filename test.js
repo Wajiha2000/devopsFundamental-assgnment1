d71d8b3451b7ab430a6077c18d6f6d2132b3fb3c
wajiha <wajihaahmed1225@gmail.com>
2026-09-22 00:39:29 +0500

    Add automated application test

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 const assert = require("assert");
 
 const message = "DevOps Demo Application is running!";
-assert.strictEqual(message, "DevOps Demo Application is running!");
 
-const healthResponse = { status: "healthy" };
-assert.strictEqual(healthResponse.status, "healthy");
+assert.strictEqual(
+    message,
+    "DevOps Demo Application is running!"
+);
 
 console.log("All tests passed!");
